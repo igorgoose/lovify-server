f656b722cb140f43c6cc6da4d5449262284ceb31
igorgoose <igorgood01@gmail.com>
2025-01-18 23:48:35 +0300

    create empty spring-boot project

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "lovify-server"
---
+++ src/main/java/by/lovify/LovifyServerApplication.java
@@ -0,0 +1,13 @@
+package by.lovify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LovifyServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LovifyServerApplication.class, args);
+    }
+
+}
